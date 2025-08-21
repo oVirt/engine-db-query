5fc1d458d82aa2ea7130fd8f3d12dae0510fcd23
Jean-Louis Dupond <jean-louis@dupond.be>
2025-08-21 12:56:35 +0200

    engine-db-query: don't depend on deprecated strtobool
    strtobool is deprecated in distutils.
    Copy-pasted the code from distutils as a function within the code.
    
    Signed-off-by: Jean-Louis Dupond <jean-louis@dupond.be>

--- src/engine_db_query/__init__.py
+++ src/engine_db_query/__init__.py
@@ -22,7 +22,6 @@ import hashlib
 
 
 from datetime import datetime
-from distutils.util import strtobool
 from .config import ENGINE_DB_CONF_FILE
 
 
@@ -408,6 +407,22 @@ class Database():
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.logger.debug("====== end ========")
         self.disconnect()
+
+
+def strtobool(val):
+    """Convert a string representation of truth to true (1) or false (0).
+
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    val = val.lower()
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return 1
+    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return 0
+    else:
+        raise ValueError("invalid truth value %r" % (val,))
 
 
 def load_engine_credentials():
